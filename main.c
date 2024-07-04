6398d226f5b8c4ccf03d457fff79a6aa576f583d
Sebastiaan Schneider <sebastiaanschneider@gmail.com>
2024-07-04 13:19:36 +0200

    kleine cleanup

--- main.c
+++ main.c
@@ -8,23 +8,20 @@
 // tijd bijhouden
 // optimaliseren
 
-void character_counter(filename)
-{
+void character_counter(filename) {
 
-    FILE *file = fopen(filename, "r");
+    FILE* file = fopen(filename, "r");
 
-    //if (file == NULL)
-    //{
-    //    printf("Error: could not open file %s", filename);
-    //    return 1;
-    //}
+    // Print error if no usable file
+    if (file == NULL) {
+       printf("Error: could not open file %s", filename);
+    };
 
     char buffer[1000]; // Initialize a char array named 'buffer' with size of 1000
 
     int counter;
     int i = 0, c; // c is the intermediate variable, i is the increment variable
-    while ((c = fgetc(file)) != EOF)
-    { // Read contents until it reach the end of the file
+    while ((c = fgetc(file)) != EOF) { // Read contents until it reach the end of the file
         buffer[i] = c;
         i++;
         if (isspace(c))
@@ -33,30 +30,36 @@ void character_counter(filename)
         } else {
             counter++;
         }
+    };
 
-    }
-
-    // close the file
+    // Close the file
     fclose(file);
 
     // Print the character array
     // printf("%i", i);
 
     // Free the dynamically allocated buffer
-    free(buffer);
+    // free(buffer);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    // Check if filename is given
+    if (argv[1] == NULL) {
+        printf("Error: No file argument given");
+        return 1;
+    } else {
+        // Get filename from input
+        char filename = argv[1];
 
-    char filename = argv[1];
-    // Calculate the time taken by character_counter()
-    clock_t t;
-    t = clock();
-    character_counter(filename);
-    t = clock() - t;
-    double time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
-    printf("character_counter() took %i seconds to execute \n", time_taken);
+        // Calculate the time taken by character_counter()
+        clock_t t;
+        t = clock();
+        character_counter(filename);
+        t = clock() - t;
+        double time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
 
-    return 0;
+        printf("character_counter() took %i seconds to execute \n", time_taken);
+
+        return 0;
+    };
 }
